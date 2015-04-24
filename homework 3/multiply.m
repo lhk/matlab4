81ed962e3c823f2ac18d412eeaaf6d4810ba0623
lars <privatlarsklein@googlemail.com>
2015-04-24 11:58:35 +0200

    some more code for the homework

--- homework 3/multiply.m
+++ homework 3/multiply.m
@@ -21,3 +21,45 @@ a_num*b_num
 % now polynomial
 
 poly_fft = ifft(fft([9 -3 -7 45 0 0 0 0]).*fft([-29 +98 0 100  0 0 0 0]))
+
+%%
+clc
+n=5; m=4;
+a=[9,0,-3,-7,45,0,0,0,0,0];
+b=[-29,98,0,100,0,0,0,0,0,0];
+c=zeros(2*n,1);
+for k=0:(n+m-2)
+    tmp=0;
+    for l=0:k
+        tmp = tmp + a(l+1) * b(k-l+1);
+    end
+    c(k+1) = tmp;
+end
+c
+
+res_a=0; res_b=0; res_c=0;
+for i=1:10
+    res_a = res_a + a(i)*5^(i-1);
+    res_b = res_b + b(i)*5^(i-1);
+    res_c = res_c + c(i)*5^(i-1);
+end
+eps = res_c-(res_a*res_b)
+res_a*res_b
+res_c
+
+
+%%
+
+clc
+
+p1=200*log(200)
+p2=400*log(400)
+p3=600*log(600)
+
+ges1=p1+p2+p3
+
+q1=p1
+q2=400*log(400)
+ges2=2*q1+q2
+
+
