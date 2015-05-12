258b54d4b7e45b8ba3e64eff3742dabaa6d6f79d
lars <privatlarsklein@googlemail.com>
2015-05-12 12:33:13 +0200

    fixed

--- exercise2/v2/A1.m
+++ exercise2/v2/A1.m
@@ -3,11 +3,13 @@ function A1()
 
     % eps = 1
     n = 60;
-    [ spA, f ] = discretize_pde( n, 1, 1, 5*pi/6, @(x,y) 1, @(x,y) 0, true );
-    soln = spA\f;
-    figure(1);
-    plot_solution(soln,n,[.1-0.05,.05,.3,.8]);
+    [ A, f ] = discretize_pde( n, 1, 1, 5*pi/6, @(x,y) 1, @(x,y) 0, true );
     
+    soln = A\f;
+    figure(1);
+
+    plot_solution(soln,n,[.1-0.05,.05,.3,.8]);
+    %%
     % eps = 1e-2
     n = 60;
     [ spA, f ] = discretize_pde( n, 1e-2, 1, 5*pi/6, @(x,y) 1, @(x,y) 0, true );
--- exercise2/v2/stencil.m
+++ exercise2/v2/stencil.m
@@ -6,25 +6,37 @@ function [ center, left, right, top, bottom  ] = stencil( upwind, beta, epsilon,
     h2 = h*h;
     
     if upwind
-        if cosb > 0
-            center = -4*epsilon/h2 + sinb/h + cosb/h;
-            left = epsilon/h2 - cosb/h;
-            right = epsilon/h2;
-            bottom = epsilon/h^2 - sinb/h;
-            top = epsilon/h2;
+        if cosb >= 0 && sinb >= 0
+            center = 4*epsilon/h2 + sinb/h + cosb/h;
+            left = -epsilon/h2 - cosb/h;
+            right = -epsilon/h2;
+            bottom = -epsilon/h^2 - sinb/h;
+            top = -epsilon/h2;
+        elseif cosb < 0 && sinb >= 0
+            center = 4*epsilon/h2 + sinb/h - cosb/h;
+            left = -epsilon/h2;
+            right = -epsilon/h2 + cosb/h;
+            bottom = -epsilon/h^2 - sinb/h;
+            top = -epsilon/h2;
+        elseif cosb >= 0 && sinb < 0
+            center = 4*epsilon/h2 - sinb/h + cosb/h;
+            left = -epsilon/h2 - cosb/h;
+            right = -epsilon/h2;
+            bottom = -epsilon/h^2;
+            top = -epsilon/h2 + sinb/h;
         else
-            center = -4*epsilon/h2 - sinb/h - cosb/h;
-            left = epsilon/h2;
-            right = epsilon/h2 + cosb/h;
-            bottom = epsilon/h2;
-            top = epsilon/h2 + sinb/h;
+            center = 4*epsilon/h2 - sinb/h - cosb/h;
+            left = -epsilon/h2;
+            right = -epsilon/h2 + cosb/h;
+            bottom = -epsilon/h^2;
+            top = -epsilon/h2 + sinb/h;
         end
     else
-        center = -4*epsilon/h2;
-        left = epsilon/h2 - cosb/2/h;
-        right = epsilon/h2 + cosb/2/h;
-        bottom = epsilon/h2 - sinb/2/h;
-        top = epsilon/h2 + sinb/2/h;
+        center = 4*epsilon/h2;
+        left = -epsilon/h2 - cosb/2/h;
+        right = -epsilon/h2 + cosb/2/h;
+        bottom = -epsilon/h2 - sinb/2/h;
+        top = -epsilon/h2 + sinb/2/h;
     end
 
 end
